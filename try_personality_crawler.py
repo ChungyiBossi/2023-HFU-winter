dd60a01ef9598601f77c2a75fafdb546295aeee6
Bossitojk <bossiwai@gmail.com>
2024-01-19 22:31:18 +0800

    [feature] update personality cafe crawler

--- try_personality_crawler.py
+++ try_personality_crawler.py
@@ -3,6 +3,8 @@ import csv
 import time
 import random
 import requests
+import os
+import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,7 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 
-def save_data(posts_data, pages_csv):
+def save_thread_data(posts_data, pages_csv):
     result = {
         'Author': [post['Author'] for post in posts_data],
         'Thread_Title': [post['Thread_Title'] for post in posts_data],
@@ -52,20 +54,23 @@ def crawl_thread(thread_url, driver):
     return result
 
 
-def crawl_personlitycafe_forum(base_url, author_limit, output_folder='data_personality', forum_name="istp_posts_data.csv"):
+def get_chrome_driver(is_eager=True):
     options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'eager'
+    if is_eager:
+        options.page_load_strategy = 'eager'
+    # options.add_argument('--headless')
     driver = webdriver.Chrome(
         options=options
     )
+    return driver
 
-    authors_set = set()
-    posts_data = []
+
+def get_total_page_number(forum_url):
+    driver = get_chrome_driver()
     # 初始頁面
-    url = base_url
-    driver.get(url)
+    driver.get(forum_url)
     print('Get forum web page.')
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'structItem-title'))
     )
 
@@ -78,86 +83,102 @@ def crawl_personlitycafe_forum(base_url, author_limit, output_folder='data_perso
     total_pages = int(page_nav_element.text)
     print("Total pages: ", total_pages)
 
-    for page in range(1, total_pages + 1):
-        # for page in range(1, 3):
-        # 動態生成網址
-        if page == 1:
-            url = base_url
-        else:
-            url = f"{base_url}page-{page}?sorting=latest-activity"
+    driver.quit()
 
-        print(f'{"=" * 10} Page {page} start {"=" * 10}')
-        driver.get(url)
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '//a[@class=" thread-title--gtm"]'))
-        )
-        thread_links_elements = driver.find_elements(
-            By.XPATH, '//a[@class=" thread-title--gtm"]')
+    return total_pages
 
-        # To solve bug: Message: stale element reference: stale element not found
-        # need to store thread links to prevent bug above
-        links = list()
-        for elements in thread_links_elements:
-            link = elements.get_attribute('href')
-            links.append(link)
 
-        print(f'{len(links)} threads find')
-        for i, thread_url in enumerate(links):
+def collect_thread_urls(base_url, forum_name, thread_limit):
+    thread_urls = list()
+    total_pages = get_total_page_number(base_forum_url)
+    with get_chrome_driver() as driver:
+        for page in range(1, total_pages + 1):
+            # Stop condition
+            if thread_limit < len(thread_urls):
+                print('Exceed thread limit, collection complete')
+                break
+            print(f'{"=" * 10} Page {page} start {"=" * 10}')
+            # 動態生成網址
+            if page == 1:
+                url = base_url
+            else:
+                url = f"{base_url}page-{page}?sorting=latest-activity"
+            driver.get(url)
+            time.sleep(10)
+            # WebDriverWait(driver, 30).until(
+            #     EC.presence_of_element_located(
+            #         (By.XPATH, '//a[@class=" thread-title--gtm"]'))
+            # )
+            thread_links_elements = driver.find_elements(
+                By.XPATH, '//a[@class=" thread-title--gtm"]')
+
+            # 模擬滾動以加載更多帖子，這部分可視情況選擇是否需要
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+            # To solve bug: Message: stale element reference: stale element not found
+            # need to store thread links to prevent bug above
+            links = list()
+            for elements in thread_links_elements:
+                link = elements.get_attribute('href')
+                links.append(link)
+
+            thread_urls += links
+            time.sleep(random.randint(1, 5))
+            print(f'Collect {len(thread_urls)} threads.')
+            print(f'{"=" * 10} Page {page} end {"=" * 10}')
+        driver.quit()
+
+    threads = {
+        'thread_url': thread_urls
+    }
+    pd.DataFrame(threads).to_csv(f'data_personality/{forum_name}_threads_url.csv',
+                                 index=False, quoting=csv.QUOTE_NONNUMERIC)
+
+    return thread_urls
+
+
+def crawl_personlitycafe_forum(thread_urls, forum_name):
+    posts_data = list()
+    with get_chrome_driver() as driver:
+        for i, thread_url in enumerate(thread_urls):
             try:
-                # random sleep
-                time.sleep(random.randint(1, 5))
-                # get thread content
-                post_content = crawl_thread(thread_url, driver)
-                # 檢查是否已經爬取過該作者的貼文
-                # if post_content['Author'] not in authors_set:
-                authors_set.add(post_content['Author'])
-                # 處理該作者的貼文內容
-                posts_data.append(post_content)
-
-                # 如果已經達到指定作者數目，則退出迴圈
-                if len(authors_set) >= author_limit:
-                    break
+                time.sleep(random.randint(1, 5))  # random sleep
+                post_content = crawl_thread(
+                    thread_url, driver)  # get thread content
+                posts_data.append(post_content)  # 處理該作者的貼文內容
             except TimeoutException:
                 print(
                     f"TimeoutException: Timed out on thread {i + 1}. Skipping...")
-                pass
+                time.sleep(30)
+                continue
+            except Exception as e:
+                print("Something wrong:", e)
+                print(f"Save log and current index.{i}")
+                with open(f'checkpoint_{time.asctime()}.json', 'w') as cp:
+                    checkpoint = {
+                        'forum_name': forum_name,
+                        'current_index': i
+                    }
+                    cp.write(json.dumps(checkpoint))
+
             time.sleep(random.randint(1, 5))
 
-        # 如果已經達到指定作者數目，則退出迴圈
-        if len(authors_set) >= author_limit:
-            print(
-                f'{len(authors_set)} meet the author limit, finishing the crawling....')
-            break
-        else:
-            print(f'Find author {len(authors_set)}, continue')
-        # 模擬滾動以加載更多帖子，這部分可視情況選擇是否需要
-        driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);")
-
-        print(f'{"=" * 10} Page {page} end {"=" * 10}')
-
-        if page % 5 == 0:
-            pages_csv = f"{output_folder}/{forum_name}_{page-4}-{page}.csv"
-            save_data(posts_data=posts_data, pages_csv=pages_csv)
-            posts_data = list()
-
-    pages_csv = f"{output_folder}/{forum_name}_{page-5*(page//5)}-{page}.csv"
-    save_data(posts_data=posts_data, pages_csv=pages_csv)
-
-    driver.quit()
+            if i % 200 == 0:
+                save_thread_data(
+                    posts_data, f'data_personality/{forum_name}_{i}.csv')
+                posts_data = list()
+                driver.quit()
+                driver = get_chrome_driver()
+        driver.quit()
     # 檢查爬取到的作者數量是否足夠
-    if len(authors_set) < author_limit:
-        print(
-            f"Warning: Only crawled {len(authors_set)} unique authors, which is less than the required {author_limit}. The data may not be sufficient.")
-
+    print(f'Finish this forum: {forum_name}')
     return posts_data
 
 
 # 設定論壇基本網址
 base_forum_urls = [
     "https://www.personalitycafe.com/forums/istp-forum-the-mechanics.9/",
-    "https://www.persoalitycafe.com/forums/enfj-forum-the-givers.17/",
+    # "https://www.persoalitycafe.com/forums/enfj-forum-the-givers.17/",
     "https://www.personalitycafe.com/forums/enfp-forum-the-inspirers.19/",
     "https://www.personalitycafe.com/forums/entj-forum-the-executives.13/",
     "https://www.personalitycafe.com/forums/entp-forum-the-visionaries.15/",
@@ -175,17 +196,45 @@ base_forum_urls = [
 ]
 # base_forum_url = "https://www.personalitycafe.com/forums/istp-forum-the-mechanics.9/"
 
+# PART 1:設定要爬取的作者數目，取得所有的thread urls 並存起來
+# for base_forum_url in base_forum_urls:
+#     # 設定輸出檔案的路徑
+#     forum_name = base_forum_url.split('/')[-2]
+
+#     print('Forum url: ', base_forum_url)
+#     print('Forum name: ', forum_name)
+
+#     url_collection_filename = f'data_personality/{forum_name}_threads_url.csv'
+#     if os.path.exists(url_collection_filename):
+#         print("Thread url collections find.")
+#         thread_urls = pd.read_csv(url_collection_filename)['thread_url']
+#     else:
+#         try:
+#             # Get Thread Url
+#             print("Start Collecting")
+#             thread_urls = collect_thread_urls(base_forum_url, forum_name, 1000)
+#         except Exception as e:
+#             print("Something Wrong: ", e)
+#             print("Skipping this forum.")
+#             continue
+
+# PART 2: 爬取內容
 # 設定要爬取的作者數目
-author_limit = 500 * len(base_forum_urls)
 for base_forum_url in base_forum_urls:
+
     # 設定輸出檔案的路徑
     forum_name = base_forum_url.split('/')[-2]
-
     print('Forum url: ', base_forum_url)
     print('Forum name: ', forum_name)
     # 爬取資料
-    all_posts_data = crawl_personlitycafe_forum(
-        base_forum_url, author_limit, forum_name=forum_name)
-
-    # 將所有的資料轉換成 DataFrame
-    df_all_posts = pd.DataFrame(all_posts_data)
+    url_collection_filename = f'data_personality/{forum_name}_threads_url.csv'
+    if os.path.exists(url_collection_filename):
+        print("Thread url collections find.")
+        thread_urls = pd.read_csv(url_collection_filename)['thread_url']
+        try:
+            all_posts_data = crawl_personlitycafe_forum(
+                thread_urls, forum_name=forum_name)
+        except Exception as e:
+            print("Something Wrong: ", e)
+            print("Skipping this forum.")
+            continue
